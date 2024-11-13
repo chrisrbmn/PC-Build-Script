b9a753b365c9f825dda15aea92a7d3f7941f0880
Chris <chrisrbmn@users.noreply.github.com>
2024-11-13 16:34:39 -0600

    Complete rebuild

--- download.ps1
+++
@@ -1,6 +0,0 @@
-Set-ExecutionPolicy remotesigned
-function downloadbuilder {
-Invoke-WebRequest -Uri "https://github.com/matstocks/PC-Build-Script/archive/master.zip" -OutFile "C:\build\PCBuild.zip"
-Expand-Archive C:\build\PCBuild.zip -DestinationPath C:\build\
-}
-downloadbuilder
